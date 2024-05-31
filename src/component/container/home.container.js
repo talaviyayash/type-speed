bf8c2db7b2fdf3406b13d67c690abb24ea449272
talaviyayash <yashtalaviya654@gmail.com>
2024-05-31 19:07:04 +0530

    Change the time of app

--- src/component/container/home.container.js
+++ src/component/container/home.container.js
@@ -3,7 +3,7 @@ const allWord =
   "We debated this decision, but in our experience it is worse to leave corrupted UI in place than to completely remove it. For example, in a product like Messenger leaving the broken UI visible could lead to somebody sending a message to the wrong person. Similarly, it is worse for a payments app to display a wrong amount than to render nothing. This change means that as you migrate to React 16, you will likely uncover existing crashes in your application that have been unnoticed before. Adding error boundaries lets you provide better user experience when something goes wrong";
 
 export default function Home() {
-  const totalTime = 5;
+  const totalTime = 80;
   const [paragraph] = useState(() => allWord.split(" "));
   const [word, setWord] = useState("");
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -41,27 +41,6 @@ export default function Home() {
       setWordIsCorrect(false);
     }
   }
-  const handleSubmit = (event) => {
-    const value = event.target.value;
-    setWord(value);
-    const currentValue = paragraph[currentIndex];
-    if (value[value.length - 1] === " ") {
-      if (currentValue === value.trim()) {
-        setCorrectWord([...correctWord, currentIndex]);
-      } else {
-        setWrongWord([...wrongWord, currentIndex]);
-      }
-      setCurrentIndex(currentIndex + 1);
-      setWord("");
-      setWordIsCorrect(true);
-    } else if (value.length === 0) {
-      setWordIsCorrect(true);
-    } else if (currentValue.startsWith(value)) {
-      setWordIsCorrect(true);
-    } else {
-      setWordIsCorrect(false);
-    }
-  };
   return {
     paragraph,
     currentIndex,
